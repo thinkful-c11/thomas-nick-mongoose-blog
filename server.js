31c2e7ce62a8afe92416334ddb548470c6d4ba8e
tckastanek <tckastanek@gmail.com>
2017-06-06 16:06:11 -0400

    put complete

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/posts/:id', (req, res) => {
   .then(post => res.json(post.apiRepr()))
   .catch(err => {
     console.error(err);
-    res.status(500).json({message: 'Internal server error'})
+    res.status(500).json({message: 'Internal server error'});
   });
 });
 
@@ -46,7 +46,7 @@ app.post('/posts', (req, res) => {
   for (let i=0; i<requiredFields; i++) {
     const field = requiredFields[i];
     if(!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
+      const message = `Missing \`${field}\` in request body`;
       console.error(message);
       return res.status(400).send(message);
     }
@@ -61,14 +61,35 @@ app.post('/posts', (req, res) => {
       post => res.status(201).json(post.apiRepr()))
       .catch(err => {
         console.error(err);
-        res.status(500).json({message: `Internal server error`});
+        res.status(500).json({message: 'Internal server error'});
       });
 });
 
+app.put('/posts/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id (${req.body.id}) must match.`);
+    console.error(message);
+    res.status(400).json({ message });
+  }
 
+  const toUpdate = {};
+  const updatableFields = ['title', 'content', 'author'];
 
+  updatableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
 
-
+  BlogPost
+    .findByIdAndUpdate(req.params.id,
+       {$set: toUpdate},
+       {new: true})
+    .exec()
+    .then(post => res.status(201).json(post.apiRepr()))
+    .catch(err => res.status(500).json({message: 'Internal server error '}));
+});
 
 
 
